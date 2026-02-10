ffa8538126efb7a0aa996655db8aad9e31b69f94
hugofranco <hugopereirafranco47@gmail.com>
2026-02-10 19:05:38 -0300

    proyecto del gato y el raton, bases solidas para seguir desarrollando

---
+++ minimax_lab.py
@@ -0,0 +1,54 @@
+# le damos los valores para la cantidad de filas y columnas que tendra nuestra matriz
+filas = 6
+columnas = 6
+#creamos la matriz principal
+matriz = []
+#posiciones reales de cada cosa dentro del tablero - posicion inicial
+pos_gato = [4, 4]
+pos_raton = [0, 0]
+pos_queso = [2, 3]
+#bucle for para ir agregando puntos en todo el "mapa"
+for i in range(filas):
+    fila_temporal = []
+    for j in range(columnas):
+        fila_temporal.append(".")
+    matriz.append(fila_temporal)
+# interpreta las ubicaciones asignadas anteriormente para que puedan ser agregadas al tablero
+matriz[pos_gato[0]][pos_gato[1]] = 'G'
+matriz[pos_raton[0]][pos_raton[1]] = 'R'
+matriz[pos_queso[0]][pos_queso[1]] = '#'
+
+jugando = True
+
+while jugando:
+    #mostrar el tablero del juego con las posiciones ya establecidas antes 
+    for fila in matriz:
+        for elemento in fila:
+            print(elemento,end=" ")
+        print("  ")
+    #pedimos al usuario el input donde elige la direccion de movimiento del gato
+    print("Ingrese el movimiento que quiere realizar: w:arriba, s:abajo, a: izquierda, d: derecha, q: salir")
+    direccion = input("Su movimiento: ")
+    #se cambia la posicion del gato por el "." cada que se mueve
+    matriz[pos_gato[0]][pos_gato[1]] = '.'
+
+    if direccion == "w" and pos_gato[0] > 0: #movimiendo hacia arriba
+        pos_gato[0] = pos_gato[0] - 1
+    elif direccion == "s" and pos_gato[0] < 4: #movimiento hacia abajo 
+        pos_gato[0] = pos_gato[0] + 1
+    elif direccion == "a" and pos_gato[1] > 0: #movimiento a la izquierda
+        pos_gato[1] = pos_gato[1] - 1
+    elif direccion == "d" and pos_gato[1] < 4:
+        pos_gato[1] = pos_gato[1] + 1
+    elif direccion == "q":
+        print("Saliendo del juego...")
+        jugando = False
+    else:
+        print("El movimiento no es valido, o salio del tablero")
+
+
+    matriz[pos_gato[0]][pos_gato[1]] = "G"
+    
+
+
+    
